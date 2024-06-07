9f48412d217b5bdf184765bafb3b37e7bd2dd6cc
Juan Herrera <juan.herrerati@gmail.com>
2024-06-07 16:35:07 -0400

    Avance

--- src/actores/CrearActores.tsx
+++ src/actores/CrearActores.tsx
@@ -1,12 +1,31 @@
+import axios from "axios";
 import { FormularioActores } from "./FormularioActores";
+import { actorCracionDTO } from "./actores.model";
+import { urlActores } from "../utils/endpoints";
+import { useNavigate } from "react-router-dom";
+import MostrarErrores from "../utils/MostrarErrores";
+import { useState } from "react";
 
 export const CrearActores = (props: Props) => {
+  const [errores, setErrores] = useState<string[]>([]);
+  const history = useNavigate();
+
+  async function crear(actor: actorCracionDTO) {
+    try {
+      await axios.post(urlActores, actor);
+      history("/actores");
+    } catch (error) {
+      setErrores(error.data);
+    }
+  }
+
   return (
     <>
       <h3>Crear Actores</h3>
+      <MostrarErrores errores={errores} />
       <FormularioActores
         modelo={{ nombre: "", fechaNacimiento: undefined }}
-        onSubmit={(valores) => console.log(valores)}
+        onSubmit={async (valores) => await crear(valores)}
       />
     </>
   );
